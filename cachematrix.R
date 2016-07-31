f3e84b5e8a1a158c2bee678461d5fae76e2c98ec
Radha05 <radhika.bukka@gmail.com>
2016-07-30 23:46:44 -0700

    Added functions for peer graded assignments
    Added functions for peer graded assignments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions makeCacheMatrix() and cacheSolve() are used to cache the inverse of a
+## matrix instead of computing the inverse repeatedly, as Matrix inversion computation is 
+## costly computation usually.
 
-## Write a short comment describing this function
+## The following function creates a special matrix object that can
+## cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+       inver <- NULL
+       set <- function(y) {
+       x <<- y
+       inver <<- NULL
+  }
+       get <- function() x
+       setinver <- function(inverse)  inver <<- inverse
+       getinver <- function() inver
+    list(set = set, get = get,setinver = setinver,getinver =getinver)
+  }
+  
+## The following function computes the inverse of the special matrix 
+##returned by makeCacheMatrix. This function will retrieve the inverse 
+##from the cache if it has already been created.
 
-}
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## Return a matrix that is the inverse of 'x'
+       inver <- x$getinver()
+       if(!is.null(inver)) {
+       message("getting cached data")
+       return(inver)
+       }
+       data <- x$get()
+       inver <- solve(data, ...)
+       x$setinver(inver)
+       inver
+  }
